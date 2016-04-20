4fb41123b87d0522ef1afa519888fe9e76bcb8cd
creeperyang <yangcreeper@hotmail.com>
2016-04-20 15:59:52 +0800

    更改front/src目录结构

--- config.js
+++ config.js
@@ -3,14 +3,15 @@
 const path = require('path');
 
 const root = __dirname;
+const staticRoot = path.join(root, 'front/src');
 
 module.exports = {
     hbsOptions: {
-        viewPath: path.join(root, 'front/src/views'),
-        partialPath: path.join(root, 'front/src/views/partials'),
-        layoutPath: path.join(root, 'front/src/views/layouts'),
-        helperPath: path.join(root, 'front/src/views/helpers'),
-        dataPath: path.join(root, 'front/src/views/data'),
+        helperPath: path.join(staticRoot, 'helpers'),
+        dataPath: path.join(staticRoot, 'data'),
+        viewPath: path.join(staticRoot, 'views'),
+        partialPath: path.join(staticRoot, 'views/partials'),
+        layoutPath: path.join(staticRoot, 'views/layouts'),
         defaultLayout: 'index',
         extname: '.html',
         templateOptions: {}
@@ -18,7 +19,7 @@ module.exports = {
     hbsOnerror: (err, ctx, next) => {
         console.log('hbs render error.', err && err.message);
     },
-    staticRoot: path.join(root, 'front/src'),
+    staticRoot,
     server: {
         host: '0.0.0.0',
         port: 3000
